ca6e3734e861105bfb5eea8e96447cbc91501692
Marco <marcopa84@hotmail.it>
2020-01-20 19:38:02 +0100

    Aggiunti ulteriori menu e styling!

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ $(document).ready(function () {
   $('li').mouseenter(
   function () {
     $(this).find('.drop-menu').fadeIn();
+    $(this).children('a').addClass('link-active');
   })
 $('li').mouseleave(
   function () {
     $(this).find('.drop-menu').fadeOut();
+    $(this).children('a').removeClass('link-active');
   })
 });
