b8d5050847a55141f02d611712d407bb08dfbb75
Marco <marcopa84@hotmail.it>
2020-01-20 18:46:49 +0100

    Primo drop menu!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+$(document).ready(function () {
 
+  $('li').mouseenter(
+  function () {
+    $(this).find('.drop-menu').fadeIn();
+  })
+$('li').mouseleave(
+  function () {
+    $(this).find('.drop-menu').fadeOut();
+  })
+});
